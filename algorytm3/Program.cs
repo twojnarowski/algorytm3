ab31747de6fe4584aa0947a2f23e717d6a68e3c1
tymonomyt <tymoteusz1138@gmail.com>
2022-07-07 14:56:41 +0200

    Dodaj pliki projektów.

---
+++ algorytm3/Program.cs
@@ -0,0 +1,34 @@
+﻿foreach (string numer in args)
+{
+    bool isStrobogramatyczny = true;
+    for (int j = 0; j <= numer.Length / 2; j++)
+    {
+        isStrobogramatyczny = numer[j] switch
+        {
+            '0' or '8' or '1' => numer[j] == numer[numer.Length - 1 - j],
+            '6' => numer[numer.Length - 1 - j] == '9',
+            '9' => numer[numer.Length - 1 - j] == '6',
+            '2' => numer[numer.Length - 1 - j] == '5',
+            '5' => numer[numer.Length - 1 - j] == '2',
+            _ => false,
+        };
+        if (!isStrobogramatyczny)
+            break;
+    }
+    if (isStrobogramatyczny)
+    {
+        bool isPierwsza = true;
+        int liczba = Convert.ToInt32(numer);
+        for (int i = 2; i < Math.Sqrt(liczba); i++)
+        {
+            if (Math.DivRem(liczba, i).Remainder == 0)
+            {
+                isPierwsza = false;
+                break;
+            }
+        }
+        Console.WriteLine("Liczba " + numer + (isStrobogramatyczny ? " " : " nie ") + "jest strobogramatyczna" + (isPierwsza ? " i pierwsza! " : "!"));
+    }
+    else
+        Console.WriteLine("Liczba " + numer + (isStrobogramatyczny ? " " : " nie ") + "jest strobogramatyczna!");
+}
